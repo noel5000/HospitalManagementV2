3a53678eb84790811a508af3741c5415e0fc77ff
Noel Jimenez <noeljimenez.400@gmail.com>
2025-03-01 20:05:06 -0400

    aaa

--- source/PointOfSalesV2.FrontEnd/ClientApp/src/app/app.component.spec.ts
+++ source/PointOfSalesV2.FrontEnd/ClientApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'test' title`, () => {
+  it(`should have the 'hospital-management' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
+    expect(app.title).toEqual('hospital-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hospital-management');
   });
 });
